1c511dad28351ed7e64ff11e7bf742f00e84781a
Emidio Valeretto <emidio.valereto@gmail.com>
2024-11-17 20:27:47 +0000

    chore: create members template and implement a views to render it

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('journal.urls')),
+    path('accounts/', include('allauth.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
--- journal/views.py
+++ journal/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, 'journal/index.html')
+
+
+@login_required(login_url='index')
+def members(request):
+    return render(request, 'journal/members.html')
